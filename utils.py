cd9d5efea3e7143d4bd724be079a47a5277e1b69
inizioRUS <46847575+inizioRUS@users.noreply.github.com>
2021-08-21 20:33:11 +0400

    добавлние lemmatize с pymorphy2

--- utils.py
+++ utils.py
@@ -1,3 +1,18 @@
+import json
+import re
+from pymorphy2 import MorphAnalyzer
+from pymystem3 import Mystem
+import gensim
+import nltk
+from nltk.corpus import stopwords
+
+nltk.download("stopwords")
+stopwords_ru = stopwords.words("russian") +  json.loads(open("stopwords-ru.json").read())
+m = Mystem()
+morph = MorphAnalyzer()
+patterns = '[0-9!a-z#$%&A-Z"()*+,./:;<=>?-@[\]^_`{|}~—\"\-]+'
+
+# Вариация с pymystem3
 def lemmatize(doc, patterns, m):
     doc = re.sub(patterns, ' ', doc)
     doc = m.lemmatize(doc)
@@ -8,6 +23,17 @@ def lemmatize(doc, patterns, m):
             tokens.append(token)
     return " ".join(tokens)
 
+
+# Вариация с pymorphy2
+def lemmatize(doc, patterns, morph):
+    doc = re.sub(patterns, ' ', doc)
+    tokens = []
+    for token in doc.split():
+        if token:
+            token = token.strip()
+            doc = morph.normal_forms(doc)[0]
+            tokens.append(token)
+    return " ".join(tokens)
 
 def preprocess(text):
     result = []
