68937c936ad3341b3a6493b91676f7f57fdac176
iliakrainow <40818918+iliakrainow@users.noreply.github.com>
2021-08-21 22:21:07 +0400

    front

---
+++ flask_app.py
@@ -0,0 +1,43 @@
+from flask import Flask, app, json, render_template, request, jsonify
+from json import load
+
+from time import sleep
+
+application = Flask(__name__, static_folder='assets')
+
+@application.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'GET':
+        return render_template('index.html')
+    else:
+        file = request.files['file']
+        file.save('now.json')
+        return render_template('res.html')
+
+
+@application.route('/api/getNews')
+def getNews():
+    with open('now.json', encoding='utf-8') as file:
+        json = load(file)
+    
+
+    res = []
+    for i in json:
+        for article in i['news']:
+            res.append(article['headline'])
+    return jsonify(res)
+
+
+@application.route('/api/getName')
+def getName():
+    sleep(3)
+    return 'Вакцины от COVID-19'
+
+
+@application.route('/api/getTags')
+def getTags():
+    return jsonify('интерфакс социалист досрочный майя молдавия четверг выборы заседание глава большинство правительство пост санда утверждение президент март кишинёв парламентский кандидат парламент партия суд владимир посол дата назначение конституционный случай головатюк россия настаивать гроса премьер-министр игорь заявить новый депутат кворум состояться отсутствие фракция получить право назначить ситуация'.split())
+
+
+if __name__ == '__main__':
+    application.run()
